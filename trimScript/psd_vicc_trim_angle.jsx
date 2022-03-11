eac21090a18c86e444d7cf0b0015d66391773c17
Kaito Horio <walkingegg.8814919@gmail.com>
2022-03-11 18:43:17 +0900

    UPDATE_仮完成

--- trimScript/psd_vicc_trim_angle.jsx
+++ trimScript/psd_vicc_trim_angle.jsx
@@ -1,29 +1,96 @@
 /**
-   ユーザの変更値（ユーザ毎に変更が必要）
-**/
-// IMG_EXTENSION = "*.psd";
-// ACTION_NAME = "vicc_trim_00";
-// SET_NAME = "vicc_trim";
-
-
-/**
    下準備
 **/
 var myDoc = app.activeDocument;
+var activeDocument_name = activeDocument.name;//アクティブになっているドキュメントの名前を取得	
+
+myDoc.save(); //ファイルを上書き保存
+var inPath = myDoc.path + '/' + myDoc.name;
+var layList = getArtLayerArray(1);
 
 
-if (BridgeTalk.appName == "photoshop") {
-    angleExportSel();
+var gUIWindow = new Window('dialog', 'vicc_trimScript');
+
+gUIWindow.bounds = [200, 100, 580, 280];
+
+gUIWindow.activeDocPnl = gUIWindow.add("panel", [10, 5, 370, 90], "アクティブドキュメント");
+gUIWindow.activeDocPnl.editText = gUIWindow.activeDocPnl.add("edittext", [10, 15, 345, 33], activeDocument_name);
+gUIWindow.activeDocPnl.editText.enabled = false
+
+gUIWindow.activeDocPnl.radiorBtn1 = gUIWindow.activeDocPnl.add("radiobutton", [10, 40, 200, 58], "すべてのレイヤーを書き出し");
+gUIWindow.activeDocPnl.radiorBtn2 = gUIWindow.activeDocPnl.add("radiobutton", [10, 55, 200, 75], "選択レイヤーを書き出し");
+
+gUIWindow.activeDocPnl.radiorBtn1.value = true;
+
+gUIWindow.dList = gUIWindow.add("dropdownlist", [15, 110, 365, 130], layList);
+gUIWindow.dList.selection = 0;
+
+gUIWindow.filterPnl = gUIWindow.add("panel", [10, 90, 370, 140], "フィルターの処理");
+
+gUIWindow.okBtn = gUIWindow.add("button", [80, 145, 175, 170], "実行");
+gUIWindow.cancelBtn = gUIWindow.add("button", [190, 145, 285, 170], "キャンセル");
+
+
+gUIWindow.okBtn.onClick = function () {
+    if (gUIWindow.activeDocPnl.radiorBtn2.value) {
+
+        angleExportSel(gUIWindow.dList.selection);
+    }
+    else {
+        angleExportAll();
+    }
+
+    gUIWindow.close();
+
+}
+
+gUIWindow.cancelBtn.onClick = function () {
+    gUIWindow.close();
 }
 
 
-function angleExportSel() {
-    myDoc.save(); //ファイルを上書き保存
-    var inPath = myDoc.path + '/' + myDoc.name;
-    var outPath = selectFolder("処理後のファイルを保存するフォルダを選択");
-    var layList = getArtLayerArray(1);
+/**
+   メイン
+**/
+if (BridgeTalk.appName == "photoshop") {
+    gUIWindow.show();
+}
 
-    for (j = 1; j < layList.length; j++) {
+
+/**
+   関数↓
+**/
+function angleExportSel(layNum) {
+    var outPath = selectFolder("処理後のファイルを保存するフォルダを選択");
+    var layNum = layNum + 1;
+
+    try {
+        doOpen(inPath);
+
+        // 開いたファイルにアクションを適用
+        doAction("vicc_trim_00" + layNum, "vicc_trim");
+
+        //.psdの前の部分を抽出
+        inName = activeDocument.name;
+        inName = getTrimStr(inName, ".", 0);
+
+        // 分割されたファイルを保存する
+        saveAsJPG(outPath + "/" + inName + "_" + layList[layNum - 1] + ".jpg");
+
+        doFinsh();
+
+    } catch (e) {
+        
+    }
+
+    doOpen(inPath)
+}
+
+
+function angleExportAll() {
+    var outPath = selectFolder("処理後のファイルを保存するフォルダを選択");
+
+    for (j = 1; j <= layList.length; j++) {
         try {
             doOpen(inPath)
 
@@ -35,43 +102,13 @@ function angleExportSel() {
             inName = getTrimStr(inName, ".", 0)
 
             // 分割されたファイルを保存する
-            saveAsJPG(outPath + "/" + inName + "_" + layList[j] + ".jpg");
+            saveAsJPG(outPath + "/" + inName + "_" + layList[j - 1] + ".jpg");
 
             doFinsh();
 
         } catch (e) {
             break;
-         }
-    }
-    doOpen(inPath)
-}
-
-
-function angleExportAll() {
-    myDoc.save(); //ファイルを上書き保存
-    var inPath = myDoc.path + '/' + myDoc.name;
-    var outPath = selectFolder("処理後のファイルを保存するフォルダを選択");
-    var layList = getArtLayerArray(1);
-
-    for (j = 1; j < layList.length; j++) {
-        try {
-            doOpen(inPath)
-
-            // 開いたファイルにアクションを適用
-            doAction(ACTION_NAME + j, SET_NAME);
-
-            //.psdの前の部分を抽出
-            inName = activeDocument.name
-            inName = getTrimStr(inName, ".", 0)
-
-            // 分割されたファイルを保存する
-            saveAsJPG(outPath + "/" + inName + "_" + layList[j] + ".jpg");
-
-            doFinsh();
-
-        } catch (e) {
-            break;
-         }
+        }
     }
     doOpen(inPath)
 }
