748cb4f73da050d3a790e9fdf62cb09938bd5304
walk-ingegg <walk-ingegg@omuken.me>
2022-09-06 00:13:56 +0900

    add: psd save function

--- backupFileTimestamp/backupFileTimestamp.js
+++ backupFileTimestamp/backupFileTimestamp.js
@@ -3,19 +3,23 @@ IMG_EXTENSION = "*.psd";
 ACTION_NAME = "vicc_trim";
 SET_NAME = "vicc_trim";
 
-var myDoc = app.activeDocument;
-var inPath = myDoc.path + "/" + myDoc.name;
-myDoc.save(); //ファイルを上書き保存
+var doc = app.activeDocument;
+var docName = doc.name;
+var docPath = doc.path;
+
+var inPath = docName + "/" + docPath;
+var backupPath = docPath + "/old_" + getTrimStr(docName, ".", 0);
 
 /**
-   メイン
-**/
+ メイン
+ **/
 if (BridgeTalk.appName == "photoshop") {
   main();
 }
 
 function main() {
-  var outPath = selectFolder("処理後のファイルを保存するフォルダを選択");
+  doc.save(); //ファイルを上書き保存
+  makeDir(backupPath);
 
   try {
     // 開いたファイルにアクションを適用
@@ -28,7 +32,7 @@ function main() {
     // 分割されたファイルを保存する
     saveAsJPG(outPath + "/" + inName + ".jpg");
 
-    doFinsh();
+    doFinish();
   } catch (e) {
     alert("fail to make backup. please check problem");
   }
@@ -49,17 +53,17 @@ function doOpen(fileName) {
    JPGで保存する
    @param   ファイル名
 **/
-function saveAsJPG(Name) {
-  var fileObj = new File(Name);
-  var jpegOpt = new JPEGSaveOptions();
-  jpegOpt.embedColorProfile = true;
-  jpegOpt.quality = 12;
-  jpegOpt.formatOptions = FormatOptions.PROGRESSIVE;
-  jpegOpt.scans = 3;
-  jpegOpt.matte = MatteType.NONE;
-  activeDocument.saveAs(fileObj, jpegOpt, true, Extension.LOWERCASE);
-  fileObj = null;
-  jpegOpt = null;
+function saveAsPSD(outPath) {
+  var fileObj = new File(outPath);
+
+  var psd_Opt = new PhotoshopSaveOptions();
+  psd_Opt.layers = true; // Preserve layers.
+  psd_Opt.embedColorProfile = true; // Preserve color profile.
+  psd_Opt.annotations = true; // Preserve annonations.
+  psd_Opt.alphaChannels = true; // Preserve alpha channels.
+  psd_Opt.spotColors = true; // Preserve spot colors.
+
+  doc.saveAs(fileObj, jpegOpt, true, Extension.LOWERCASE);
 }
 
 /**
@@ -77,20 +81,29 @@ function getTrimStr(str, SPoint, SNumber) {
 /**
    ファイルを閉じる
 **/
-function doFinsh() {
+function doFinish() {
   activeDocument.close(SaveOptions.DONOTSAVECHANGES);
 }
+// function getCurrentTime() {
+//   let time;
+//   const now = new Date();
 
-const getCurrentTime = () => {
-  let time;
-  const now = new Date();
+//   const Year = now.getFullYear();
+//   const Month = str(now.getMonth() + 1).padStart(2, "0");
+//   const Day = str(now.getDate()).padStart(2, "0");
+//   const Hour = str(now.getHours()).padStart(2, "0");
+//   const Min = str(now.getMinutes()).padStart(2, "0");
+//   const Sec = str(now.getSeconds()).padStart(2, "0");
 
-  const Year = now.getFullYear();
-  const Month = str(now.getMonth() + 1).padStart(2, "0");
-  const Day = str(now.getDate()).padStart(2, "0");
-  const Hour = str(now.getHours()).padStart(2, "0");
-  const Min = str(now.getMinutes()).padStart(2, "0");
-  const Sec = str(now.getSeconds()).padStart(2, "0");
+//   return (time = "_" + Year + Month + Day + "_" + Hour + Min + Sec);
+// }
 
-  return (time = "_" + Year + Month + Day + "_" + Hour + Min + Sec);
-};
+/**
+   pathのフォルダが無ければ作る
+**/
+function makeDir(path) {
+  newFolder = new Folder(path);
+  if (!path.exists) {
+    newFolder.create();
+  }
+}
